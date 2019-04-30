be2f420e07aff003d16b796b87f541e65bae1350
Li Jin <carol.jinli@gmail.com>
2019-04-30 14:49:29 -0700

    fix FindNonRepeatingCharacter.java

--- FindNonRepeatingCharacter.java
+++ FindNonRepeatingCharacter.java
@@ -1,34 +1,47 @@
-//Java program to find first non-repeating character, use hashmap
+package collections;
 
-import java.util.HashMap; 
+//Java program to find first non-repeating character 
+//Note : hashmap is used 
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
 
 
 public class FindNonRepeatingCharacter {
    
  static HashMap<Character, Integer> hm = new HashMap<Character, Integer>(); 
    
-  static void getCharCountForArray(String str)  
+  static void getIdenticalArray(String str)  
   { 
       for (int i = 0; i < str.length();  i++) 
       { 
     	  int count = 0;
-         // If character already occurred
+         // If character already occurred,  
           if(hm.containsKey(str.charAt(i))) 
           { 
-        	  hm.remove(str.charAt(i));
+        	  count = hm.get(str.charAt(i));
           } 
-          else { //if character is never seen before
-        	  count = 1;
+          
+        	  count++;
         	  hm.put(str.charAt(i), count);  
-          }                        
+                      
       }  
-      
-      System.out.println("The non repeat character string is: "+ hm.keySet());
+      Set set1 = hm.entrySet();
+      java.util.Iterator it = set1.iterator();      
+      while(it.hasNext()) {
+    	  Map.Entry me = (Entry) it.next();
+    	  if(me.getValue().equals(1)){
+    		  System.out.print(me.getKey());
+      		}
+      }
   } 
 
+ // Driver method 
  public static void main (String[] args) 
  { 
-     String str = "geeksforgeeks"; 
-     getCharCountForArray(str); 
+     String str = "geeksforgeeksg"; 
+     getIdenticalArray(str); 
  } 
 }
