f864595732abbcc69d7d17611a7d5d8c8c4b6e1e
Li Jin <carol.jinli@gmail.com>
2019-06-13 14:56:35 -0700

    Update SuperDigit.java

--- SuperDigit.java
+++ SuperDigit.java
@@ -103,3 +103,63 @@ public class SuperDigit {
         scanner.close();
     }
 }
+
+
+//JavaScript version:
+
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// Complete the digitSum function below.
+function digitSum(n, k) {
+    if (n.length == 1 && k == 1) return parseInt(n);
+
+    let sum = 0;
+    for (let i = 0; i < n.length; i++) {
+        sum += parseInt(n.charAt(i));
+    }
+
+    sum *= k;
+
+    return digitSum(sum.toString(),1);
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const nk = readLine().split(' ');
+
+    const n = nk[0];
+
+    const k = parseInt(nk[1], 10);
+
+    const result = digitSum(n, k);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
