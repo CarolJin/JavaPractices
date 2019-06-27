7126a416e465f0d62a1325d14e005aeec6bf46b3
Li Jin <carol.jinli@gmail.com>
2019-06-27 14:46:35 -0700

    Create LinkedListInsertToTheEnd.java

---
+++ LinkedListInsertToTheEnd.java
@@ -0,0 +1,55 @@
+import java.io.*;
+import java.util.*;
+
+class Node {
+	int data;
+	Node next;
+	Node(int d) {
+        data = d;
+        next = null;
+    }
+}
+
+public class LinkedListInsertToTheEnd {
+
+    public static Node insert(Node head,int data) {
+        if(head == null) {
+            head = new Node(data);
+            return head;
+        }
+
+        Node newNode = new Node(data);
+
+        Node start = head;
+
+        while(start.next!=null) {
+            start= start.next;
+        }
+
+        start.next = newNode;
+        newNode.next = null;
+            
+        return head;
+    }
+
+	public static void display(Node head) {
+        Node start = head;
+        while(start != null) {
+            System.out.print(start.data + " ");
+            start = start.next;
+        }
+    }
+
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
+        Node head = null;
+        int N = sc.nextInt();
+
+        while(N-- > 0) {
+            int ele = sc.nextInt();
+            head = insert(head,ele);
+        }
+        display(head);
+        sc.close();
+    }
+}
