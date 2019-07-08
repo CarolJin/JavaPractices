3df3dc1301426f74d3b20fe15681709633774e0f
Li Jin <carol.jinli@gmail.com>
2019-07-08 14:22:34 -0700

    Update LinkedListInsertToTheEnd.java

--- LinkedListInsertToTheEnd.java
+++ LinkedListInsertToTheEnd.java
@@ -31,6 +31,26 @@ public class LinkedListInsertToTheEnd {
             
         return head;
     }
+	
+/*
+public static  Node insert(Node head,int data)
+     {
+        Node p=new Node(data);			
+        if(head==null)
+            head=p;
+        else if(head.next==null)
+            head.next=p;
+        else
+        {
+            Node start=head;
+            while(start.next!=null)
+                start=start.next;
+            start.next=p;
+
+        }
+        return head;
+}
+*/
 
 	public static void display(Node head) {
         Node start = head;
